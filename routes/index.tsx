fa02f994b23f3ba5af67264f3bb3cd60513bf613
yanskun <43776161+yanskun@users.noreply.github.com>
2022-08-16 22:01:15 +0900

    feat: finish dynamic route

---
+++ routes/index.tsx
@@ -0,0 +1,19 @@
+/** @jsx h */
+import { h } from "preact";
+import Counter from "../islands/Counter.tsx";
+
+export default function Home() {
+  return (
+    <div>
+      <img
+        src="/logo.svg"
+        height="100px"
+        alt="the fresh logo: a sliced lemon dripping with juice"
+      />
+      <p>
+        Welcome to `fresh` cource by Bitfumes
+      </p>
+      <Counter start={3} />
+    </div>
+  );
+}
